9acbe0db363afac1d0a509613db6945862251fb1
immortalWebDev <pgbadgujar007@gmail.com>
2024-08-23 01:12:31 +0530

    Updated code of maintainance page

--- vite-user/src/App.jsx
+++ vite-user/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import AppRouter from './components/Routes/AppRouter';
-import MaintenancePage from './components/MaintenancePage'; // Import the MaintenancePage component
+import MaintenancePage from './components/MaintainancePage/MaintainancePage';
 import './App.css';
 
 const App = () => {
--- vite-user/src/components/MaintainancePage/MaintainancePage.jsx
+++ vite-user/src/components/MaintainancePage/MaintainancePage.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import './MaintenancePage.css'; 
+import './MaintainancePage.css'
 
 
-const MaintenancePage = () => {
+const MaintainancePage = () => {
   return (
     <div className="maintenance-container">
-      <h1>Site Under Maintenance</h1>
-      <p>We are currently performing scheduled maintenance. Please check back later.</p>
+      <h1>🚧 Site Under Maintenance 🚧</h1>
+      <p>We are currently performing scheduled maintenance.</p>
+      <blockquote>
+        <p>"The best way to predict the future is to create it."</p>
+        <footer>— Peter Drucker</footer>
+      </blockquote>
+      <p className="team-signature">- Team Piyush</p>
     </div>
   );
 };
 
-export default MaintenancePage;
+export default MaintainancePage;
