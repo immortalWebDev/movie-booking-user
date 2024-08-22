2e0c7c71659db0f89628e10935ae546984a787da
immortalWebDev <pgbadgujar007@gmail.com>
2024-08-23 01:05:15 +0530

    Added Maintainance page

--- vite-user/src/App.jsx
+++ vite-user/src/App.jsx
@@ -3,14 +3,23 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import AppRouter from './components/Routes/AppRouter';
+import MaintenancePage from './components/MaintenancePage'; // Import the MaintenancePage component
 import './App.css';
 
 const App = () => {
+  const isMaintenanceMode = import.meta.env.VITE_MAINTENANCE_MODE === 'true';
+
   return (
     <Router>
-      <Header />
-      <AppRouter />
-      <Footer />
+      {isMaintenanceMode ? (
+        <MaintenancePage />
+      ) : (
+        <>
+          <Header />
+          <AppRouter />
+          <Footer />
+        </>
+      )}
     </Router>
   );
 }
---
+++ vite-user/src/components/MaintainancePage/MaintainancePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MaintenancePage.css'; 
+
+
+const MaintenancePage = () => {
+  return (
+    <div className="maintenance-container">
+      <h1>Site Under Maintenance</h1>
+      <p>We are currently performing scheduled maintenance. Please check back later.</p>
+    </div>
+  );
+};
+
+export default MaintenancePage;
