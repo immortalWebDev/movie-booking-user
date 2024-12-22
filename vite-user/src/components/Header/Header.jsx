50ddaf9aa1f8d926c91e139a06d3126a135cc5dc
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-22 15:56:43 +0530

    Added cdn img links for logos

--- vite-user/src/components/Header/Header.jsx
+++ vite-user/src/components/Header/Header.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import { Link,NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
   return (
     <header className="header">
-      <h1>Movie Booking</h1>
+       <div className="logo-container">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3496/3496625.png"
+          alt="Logo"
+        />
+        <h1>Cinema Sphere</h1>
+      </div>
+      
       <nav>
-        {/* <Link to="/">Home</Link> */}
+      <img
+          src="https://cdn-icons-png.flaticon.com/512/195/195492.png"
+          alt="Logo"
+        />
         <NavLink
           to="/"
           className={({ isActive }) => (isActive ? "active-link" : "")}        >
           Home
         </NavLink>
+       
       </nav>
     </header>
   );
